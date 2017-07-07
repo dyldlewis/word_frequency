887561030df7e8d81f421b7334d1ddfdc8f43743
Dylan Lewis <dyldlewis@gmail.com>
2017-07-07 12:34:38 -0700

    modify function to account for 5th spec - test passed

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -3,6 +3,10 @@
     {
         function countRepeats($word_to_count, $string_to_use)
         {
+            if (is_int($word_to_count)) {
+                return "Please enter words";
+            }
+
             $word_to_count = strtolower($word_to_count);
             $string_to_use = strtolower($string_to_use);
             $string_to_use = explode(" ", $string_to_use);
